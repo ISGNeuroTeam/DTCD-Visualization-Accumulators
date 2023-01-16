a4af6cea7c056e011fa50261ea3a9808255ff122
Alexandr-Bragin <abragin@isgneuro.com>
2023-01-16 22:48:12 +0900

    Stater files

---
+++ DTCD-Visualization-Accumulators/src/PluginComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="VisualizationAccumulators">
+    <div v-if="title" class="Header">
+      <div class="Title">{{ title }}</div>
+    </div>
+    <div class="Body">
+      <div class="NoData">
+        <span class="FontIcon name_infoCircleOutline Icon"></span>
+        Нет данных для отображения
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PluginComponent',
+  data() {
+    return {
+      title: 'Accumulators',
+      dataset: [],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './styles/VisualizationAccumulators.scss';
+</style>
